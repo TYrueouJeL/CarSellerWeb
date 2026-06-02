e214f36ed561863315e3aab18c40baa28785675a
TYrueouJeL <guerinremi.pro@gmail.com>
2026-06-02 13:35:12 +0200

    fix matomo

--- web/app/app.vue
+++ web/app/app.vue
@@ -15,7 +15,7 @@ useHead({
         _paq.push(['trackPageView']);
         _paq.push(['enableLinkTracking']);
         (function() {
-          var u="https://10.192.95.252:8090/";
+          var u="https://matomo.carseller.fr/";
           _paq.push(['setTrackerUrl', u+'matomo.php']);
           _paq.push(['setSiteId', '1']);
           var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
